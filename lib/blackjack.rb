35c1a1c5d066605685f9dd868ce7cea49e98cdcd
sammcgrail <sammcgrail@gmail.com>
2015-06-08 15:44:43 -0400

    add score and ace handling (fix instance variables)

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -9,7 +9,7 @@ puts "Welcome to blackjack!"
 dealer = Hand.new
 player = Hand.new
 
-players = {'Dealer' => dealer, 'Player' => player}
+@players = {'Player' => player, 'Dealer' => dealer}
 
 deck = Deck.new
 
@@ -18,19 +18,25 @@ def card_dealt(player_name, card)
   puts "#{player_name} was dealt: #{card}"
 end
 
-#deal cards to players one at a time
-count = 0
-deck.shuffle
-while count < (players.length)*2
-  players.each do |player_name,hand|
-    card = deck.deal
-    hand.hand << card
-    card_dealt(player_name, card.suit + "" + card.value)
-    count +=1
-  end
+def player_score(player_name)
+
+  player_score = @players[player_name].score
+  puts "#{player_name} score: #{player_score}"
 end
 
 
+#deal cards to @players one at a time
+count = 0
+deck.shuffle
+while count < (@players.length)*15
+  @players.each do |player_name,hand|
+    card = deck.deal
+    hand.hand << card
+    card_dealt(player_name, card.suit + "" + card.value)
+    player_score(player_name)
+    count +=1
+  end
+end
 
 
 # puts "Hit or stand (H/S):"
--- lib/hand.rb
+++ lib/hand.rb
@@ -8,22 +8,24 @@ class Hand
 
   def score
     if @hand.length > 0
-      aces = 0
+      @aces = 0
+      @score = 0
       @hand.each do |card|
         unless card.point == 'ace'
           @score += card.point
         else
-          aces +=1
+          @aces +=1
         end
       end
-      #handle aces----------------
-      while aces > 0
-        if (score + 11) <= 21 && aces < 2
-          score += 11
+      #handle @aces----------------
+      while @aces > 0
+      #   # binding.pry
+        if (@score + 11) <= 21 && @aces < 2
+          @score += 11
         else
-          score += 1
-        aces -=1
+          @score += 1
         end
+        @aces = @aces - 1
       end
       #--------------------------
     else
