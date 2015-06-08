b84b09a55eff4074fb7464753f27320b92c32371
sammcgrail <sammcgrail@gmail.com>
2015-06-08 15:20:55 -0400

    separate methods to separate files, and begin forming output message

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,86 +1,53 @@
 #!/usr/bin/env ruby
+require 'pry'
+require_relative 'card'
+require_relative 'deck'
+require_relative 'hand'
 
 puts "Welcome to blackjack!"
 
- SUITS = %w[♦ ♣ ♠ ♥]
- VALUES = %w[2 3 4 5 6 7 8 9 10 J Q K A]
+dealer = Hand.new
+player = Hand.new
 
-class Card
-  attr_reader :suit, :value
+players = {'Dealer' => dealer, 'Player' => player}
 
-  def initialize(suit, value)
-    @suit = suit
-    @value = value
-    @point = 0
-  end
+deck = Deck.new
 
-  def point
-    if not ['J', 'Q', 'K', 'A'].include? @value
-      @point = @value.to_i
-    elsif not @value == 'A'
-      @point = 10
-    else
-      @point = 'ace'
-    end
-  end
 
+def card_dealt(player_name, card)
+  puts "#{player_name} was dealt: #{card}"
 end
 
-class Deck
-  attr_reader :deck
-  def initialize
-    @deck = []
-    SUITS.each do |suit|
-      VALUES.each do |value|
-        deck << Card.new(suit,value)
-      end
-    end
-  end
-
-  def deck_size
-    @deck.length
-  end
-
-  def shuffle
-    @deck = @deck.shuffle
-  end
-
-  def deal
-    @deck.shift
+#deal cards to players one at a time
+count = 0
+deck.shuffle
+while count < (players.length)*2
+  players.each do |player_name,hand|
+    card = deck.deal
+    hand.hand << card
+    card_dealt(player_name, card.suit + "" + card.value)
+    count +=1
   end
 end
 
-class Hand
-  attr_accessor :hand, :score
-  attr_reader :points
-  def initialize
-    @hand = []
-    @score = 0
-  end
 
-  def score
-    if @hand.length > 0
-      aces = 0
-      @hand.each do |card|
-        unless card.point == 'ace'
-          @score += card.point
-        else
-          aces +=1
-        end
-      end
-      #handle aces----------------
-      while aces > 0
-        if (score + 11) <= 21 && aces < 2
-          score += 11
-        else
-          score += 1
-        aces -=1
-        end
-      end
-      #--------------------------
-    else
-      "you ain't got a hand"
-    end
-    @score
-  end
-end
+
+
+# puts "Hit or stand (H/S):"
+#
+#
+#
+#
+#
+#
+# # Player was dealt
+# # Player score:
+# #
+# #
+# # Dealer was dealt
+# # Dealer score:
+# #
+# #
+# #
+# # You win!
+# # Bust! You lose...
