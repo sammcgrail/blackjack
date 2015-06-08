9c0e133dbae23b751477531d47f49e2db5b23bb4
sammcgrail <sammcgrail@gmail.com>
2015-06-08 13:15:49 -0400

    up to shuffled deck, test unfinished

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,9 +6,31 @@
  VALUES = %w[2 3 4 5 6 7 8 9 10 J K Q A]
 
 class Card
+  attr_reader :suit, :value
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
+  end
 end
 
 class Deck
+  attr_reader :deck
+  def initialize
+    @deck = []
+    SUITS.each do |suit|
+      VALUES.each do |value|
+        deck << Card.new(suit,value)
+      end
+    end
+  end
+
+  def deck_size
+    @deck.length
+  end
+
+  def shuffle
+    @deck.shuffle!
+  end
 end
 
 class Hand
--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -14,3 +14,32 @@ describe Card do
     expect(testcard.value).to eq("6")
   end
 end
+
+describe Deck do
+  describe "#initialize" do
+    it 'has 52 card objects' do
+      testdeck = Deck.new()
+      expect(testdeck.deck_size).to eq(52)
+      # expect(testdeck.).to eq(52)
+    end
+  end
+  describe "#shuffle" do
+    it 'shuffles the deck' do
+      testdeck = Deck.new()
+      suit = testdeck.deck[0].suit
+      value = testdeck.deck[0].value
+      testdeck.shuffle
+      expect(testdeck.deck[0].suit).not_to eq(suit)
+      expect(testdeck.deck[0].value).not_to eq(value)
+    end
+  end
+end
+
+
+# describe Hand do
+#   it 'has 2 card objects' do
+#     testdeck = Deck.new()
+#     expect(testdeck.deck_size).to eq(52)
+#     # expect(testdeck.).to eq(52)
+#   end
+# end
