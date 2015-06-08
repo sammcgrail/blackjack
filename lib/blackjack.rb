2f9aeea46fff6571a1e2597f003321d7ecd14850
sammcgrail <sammcgrail@gmail.com>
2015-06-08 17:24:35 -0400

    write methods for blackjack logic

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -4,56 +4,98 @@ require_relative 'card'
 require_relative 'deck'
 require_relative 'hand'
 
-puts "Welcome to blackjack!"
+puts "Welcome to blackjack!\n\n"
 
-dealer = Hand.new
-player = Hand.new
+@dealer = Hand.new
+@player = Hand.new
 
-@players = {'Player' => player, 'Dealer' => dealer}
+@players = {'Player' => @player, 'Dealer' => @dealer}
 
-deck = Deck.new
+@deck = Deck.new
 
 
 def card_dealt(player_name, card)
-  puts "#{player_name} was dealt: #{card}"
+  puts "#{player_name} was dealt: #{card}\n"
 end
 
 def player_score(player_name)
-
   player_score = @players[player_name].score
-  puts "#{player_name} score: #{player_score}"
+  puts "#{player_name} score: #{player_score}\n\n"
+  player_score
 end
 
+def get_player_choice
+    puts "Hit or stand (H/S):"
+    player_choice = gets.chomp.to_s
+    while player_choice == 'H'
+      card = @deck.deal
+      @player.hand << card
+      card_dealt("Player",card.suit + "" + card.value)
+      score = player_score("Player")
+      if score < 21
+        puts "Hit or stand (H/S):"
+        player_choice = gets.chomp.to_s
+      else
+        return score
+      end
+    end
+    score = player_score("Player")
+end
 
-#deal cards to @players one at a time
-count = 0
-deck.shuffle
-while count < (@players.length)*15
-  @players.each do |player_name,hand|
-    card = deck.deal
-    hand.hand << card
-    card_dealt(player_name, card.suit + "" + card.value)
-    player_score(player_name)
-    count +=1
+def get_dealer_choice
+  if @dealer.score < 17
+    card = @deck.deal
+    @dealer.hand << card
+    card_dealt("Dealer",card.suit + "" + card.value)
+    player_score("Dealer")
+  else
+    puts "Dealer stays at #{@dealer.score}\n\n"
+  end
+  @dealer.score
+end
+
+def settle_score(player_score)
+  if player_score > 21
+    puts "Dealer wins!"
+  else
+    dealer_score = get_dealer_choice
+    while dealer_score <= 17
+      dealer_score = get_dealer_choice
+    end
+
+    if dealer_score > player_score && dealer_score <= 21
+      puts "Dealer wins!"
+    elsif dealer_score == player_score
+      puts "Dealer wins!"
+    else
+      puts "Player wins!"
+    end
+
+
   end
 end
 
 
-# puts "Hit or stand (H/S):"
-#
-#
-#
-#
-#
-#
-# # Player was dealt
-# # Player score:
-# #
-# #
-# # Dealer was dealt
-# # Dealer score:
-# #
-# #
-# #
+#deal cards to @players one at a time
+def deal
+  count = 0
+  @deck.shuffle
+  while count < (@players.length)*2
+    @players.each do |player_name,hand|
+      card = @deck.deal
+      hand.hand << card
+      card_dealt(player_name, card.suit + "" + card.value)
+      player_score(player_name)
+      count +=1
+    end
+  end
+end
+
+deal
+player_score = get_player_choice
+settle_score(player_score)
+# puts dealer_score = get_dealer_choice
+
+
 # # You win!
 # # Bust! You lose...
